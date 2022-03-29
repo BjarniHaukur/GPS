168990dba6e47e5fcf580d8b8f78410b49f3318f
Bobbi11 <bjarnihaukur11@gmail.com>
2022-03-29 13:13:28 +0000

    refact

--- Satelite.py
+++ Satelite.py
@@ -1,11 +1,11 @@
 import math
 import numpy as np
-
+from abc import ABC, abstractmethod
 from Methods import GaussNewton
 
 
 
-class StaticSatelite:
+class Satelite(ABC):
 
     earth_radius: int = 6371
 
@@ -14,11 +14,22 @@ class StaticSatelite:
         self.B = B
         self.C = C
 
+   
+    @abstractmethod
     def solve(self, At: float, Bt: float, Ct: float) -> tuple[float]:
-        print("solved")
+        """ 
+            Solves the system of equations according to the given travel times
+            and the current positions of the satelites.
+        """
+        ...
+
+class StaticSatelite(Satelite):
+
+    def solve(self, At: float, Bt: float, Ct: float) -> tuple[float]:
+        print("static")
 
 
-class Satelite(StaticSatelite):
+class DynamicSatelite(Satelite):
 
     def __init__(self, phi: float, theta: float,
                  offset_phi: float = 0, offset_theta: float = 0, altitude: int = 20200) -> "Satelite":
@@ -29,7 +40,10 @@ class Satelite(StaticSatelite):
         self.offset_phi = offset_phi
         self.offset_theta = offset_theta
     
-    # t/T fyrir réttan tíma
+    def solve(self, At: float, Bt: float, Ct: float) -> tuple[float]:
+        print("dynamic")
+
+    # time/T fyrir réttan tíma
     def getPos(self, time: float) -> tuple[float]:
 
         phi, theta = self.phi*time+self.offset_phi, self.theta*time+self.offset_theta
@@ -41,24 +55,24 @@ class Satelite(StaticSatelite):
         return (self.altitude*x for x in pos)
 
 
-# stat = StaticSatelite(1,2,3)
-# stat.solve(1,2,3)
-# sat = Satelite(1,2)
-# sat.solve(1,2,3)
-# print(Satelite.earth_radius)
+stat = StaticSatelite(1,2,3)
+stat.solve(1,2,3)
+dyn = DynamicSatelite(1,2)
+dyn.solve(1,2,3)
+print(Satelite.earth_radius)
 
-def main():
-    x0 = np.array((0, 0, 0))
-    centers1 = np.array([(0, 1, 1), (1,1, 1), (0,-1, 100)])
-    radii1 = np.array([1, 1, 1])
-    # centers2 = np.array([(-1,0), (1,1), (1,-1)])
-    # radii2 = np.array([1, 1, 1])
+# def main():
+#     x0 = np.array((0, 0, 0))
+#     centers1 = np.array([(0, 1, 1), (1,1, 1), (0,-1, 100)])
+#     radii1 = np.array([1, 1, 1])
+#     # centers2 = np.array([(-1,0), (1,1), (1,-1)])
+#     # radii2 = np.array([1, 1, 1])
     
 
-    x1 = GaussNewton(x0, centers1, radii1)
-    print(f"Least square distance for example 1 is at: {x1}")
-    # x2 = GaussNewton(x0, centers2, radii2)
-    # print(f"Least square distance for example 2 is at: {x2}")
+#     x1 = GaussNewton(x0, centers1, radii1)
+#     print(f"Least square distance for example 1 is at: {x1}")
+#     # x2 = GaussNewton(x0, centers2, radii2)
+#     # print(f"Least square distance for example 2 is at: {x2}")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
