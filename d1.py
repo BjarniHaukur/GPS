d0501a9371ec4b42dbe545b6885152bd85130639
unknown <natidemisket@gmail.com>
2022-03-31 19:00:56 +0000

    gauss newton

--- d1.py
+++ d1.py
@@ -11,9 +11,11 @@ if __name__ == '__main__':
     sat4 = SateliteConnection(19170,610,18390,0.07242)
     
     sys = SateliteSystem(*(sat1, sat2, sat3,sat4))
-    r_pos = sys.solve(np.array([0,0,6370,0]))
+    r_pos = sys.solve2(np.array([0,0,6370,0]))
     print(f"x= {r_pos[0]}, y= {r_pos[1]}, z= {r_pos[2]}, d= {r_pos[3]}")
     plot_satelites(sys, r_pos, name="d1")
+    d1_sol = np.array([-41.77271,-16.78919,6370.0596, 0.003201566])
+    print("diff: ", np.sum(np.sqrt((d1_sol - r_pos)**2) ))
 
 
 
